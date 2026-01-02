d5d5500c203edf3009c4f45eeb4ff1eff8966000
Danish <iryandydanish@gmail.com>
2026-01-02 21:17:14 +0800

    [preprod] - Added BentoPDF application
    [01/01/2026] Added docker compose for BentoPDF into preprod.

--- preprod-down.sh
+++ preprod-down.sh
@@ -10,4 +10,5 @@ docker compose \
   -p "$PROJECT_NAME" \
   --env-file stacks/preprod/versions.env \
   -f stacks/preprod/aiostreams/compose.yml \
+  -f stacks/preprod/bentopdf/compose.yml \
   down --remove-orphans
