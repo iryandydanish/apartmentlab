2effbe98d463d4a5b2563ed3d2de003e2a8640ef
Danish <iryandydanish@gmail.com>
2026-01-02 23:02:43 +0800

    added if statements to apps-up.sh to check deploy depending on the environments

--- apps-up.sh
+++ apps-up.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
-PROJECT_NAME="apartmentlab-prod"
-BASE_DIR="/apartmentlab/prod-repo/apartmentlab"
+BASE_DIR="$pwd"
 
-cd "$BASE_DIR"
+if [BASE_DIR=="/apartmentlab/prod-repo/apartmentlab"]; then
 
-docker compose \
-  -p "$PROJECT_NAME" \
-  --env-file versions.env \
-  -f application/aiostreams/compose.yml \
-  -f application/bentopdf/compose.yml \
-  up -d
+  PROJECT_NAME="apartmentlab-prod"
 
+  cd "$BASE_DIR"
+  docker compose \
+    -p "$PROJECT_NAME" \
+    --env-file versions.env \
+    -f application/aiostreams/compose.yml \
+    -f application/bentopdf/compose.yml \
+    up -d
 
+  echo "Applications started in production environment."
+
+elif [BASE_DIR=="/apartmentlab/preprod-repo/apartmentlab"]; then
+
+  PROJECT_NAME="apartmentlab-preprod"
+
+  cd "$BASE_DIR"
+  docker compose \
+    -p "$PROJECT_NAME" \
+    --env-file versions.env \
+    -f application/aiostreams/compose.yml \
+    -f application/bentopdf/compose.yml \
+    up -d
+
+  echo "Applications started in production environment."
+
+else
+
+  echo "Error: Unknown BASE_DIR. Please check the configuration."
+
+fi
