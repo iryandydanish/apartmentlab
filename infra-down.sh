cd804520476b333cdf58eb592edbbfe41bf550ab
Danish <iryandydanish@gmail.com>
2026-01-02 13:22:56 +0800

    [preprod] - changes to infra-down.sh
    [01/01/2026] Modified infra-down.sh to shut down portainer and cloudflared.
    
    Edit: added env file so shut down does not fail.

--- infra-down.sh
+++ infra-down.sh
@@ -6,6 +6,7 @@ cd "$BASE_DIR"
 
 docker compose \
   -p apartmentlab-infra\
+  --env-file stacks/prod/versions.env \
   -f stacks/prod/cloudflared/compose.yml \
   -f stacks/prod/portainer/compose.yml \
   down --remove-orphans
